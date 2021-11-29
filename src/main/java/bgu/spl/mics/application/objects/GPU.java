7ba5ed8bffb8518e62e626244299e1444f36dd9b
Gil <gil4390@gmail.com>
2021-11-29 18:09:10 +0200

    gil

--- src/main/java/bgu/spl/mics/application/objects/GPU.java
+++ src/main/java/bgu/spl/mics/application/objects/GPU.java
@@ -1,5 +1,7 @@
 package bgu.spl.mics.application.objects;
 
+import bgu.spl.mics.application.services.GPUService;
+
 /**
  * Passive object representing a single GPU.
  * Add all the fields described in the assignment as private fields.
@@ -12,5 +14,41 @@ public class GPU {
     enum Type {RTX3090, RTX2080, GTX1080}
 
     private Type type;
+    private Model model;
+    private Cluster cluster;
 
+    private DataBatch[] processedDataBatch;
+    private DataBatch[] unProcessedDataBatch;
+    private int indexPDB;
+
+    public GPU(Type type){
+        this.type=type;
+        switch(type){
+            case GTX1080: processedDataBatch = new DataBatch[8];
+            case RTX2080: processedDataBatch = new DataBatch[16];
+            case RTX3090: processedDataBatch = new DataBatch[32];
+        }
+        model = null;
+        indexPDB = 0;
+    }
+
+    public void Initialize(Cluster cluster){
+        this.cluster = cluster;
+    }
+
+    public void ReciveEvent(Model model){
+        this.model = model;
+    }
+
+    private void TrainModel(){
+
+    }
+
+    private void DivideDataBatch(){
+
+    }
+
+    public void ReciveProcessedData(DataBatch databatch){
+
+    }
 }
