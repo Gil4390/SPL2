6d2fffac929dd465a960962ec8fce30fac341eda
Gil Khais <gil4390@gmail.com>
2021-12-04 17:18:00 +0200

    gil

--- src/test/java/FutureTest.java
+++ src/test/java/FutureTest.java
@@ -1,15 +1,11 @@
-package src;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FutureTest {
 
-    private Future f;
-
-    @Before
     public void setUp() throws Exception {
-        f = new Future<>();
+
     }
 
     @Test
