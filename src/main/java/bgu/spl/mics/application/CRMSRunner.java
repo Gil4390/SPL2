87437598b37a2af3aba71af579910b42bc6c86c9
Gil Khais <gil4390@gmail.com>
2021-12-05 13:35:31 +0200

    main input

--- src/main/java/bgu/spl/mics/application/CRMSRunner.java
+++ src/main/java/bgu/spl/mics/application/CRMSRunner.java
@@ -1,5 +1,12 @@
 package bgu.spl.mics.application;
 
+import com.google.gson.Gson;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
 /** This is the Main class of Compute Resources Management System application. You should parse the input file,
  * create the different instances of the objects, and run the system.
  * In the end, you should output a text file.
@@ -7,5 +14,33 @@ package bgu.spl.mics.application;
 public class CRMSRunner {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        String gpus="";
+        try {
+            // create Gson instance
+            Gson gson = new Gson();
+            Gson gson2 = new Gson();
+
+            // create a reader
+            Reader reader = Files.newBufferedReader(Paths.get("C:\\Users\\Gil Khais\\Desktop\\SPL2\\example_input.json"));
+
+            // convert JSON file to map
+            Map<?, ?> map = gson.fromJson(reader, Map.class);
+
+            // print map entries
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + "=" + entry.getValue());
+                if(entry.getKey().equals("GPUS")){
+                    gpus = entry.getValue().toString();
+                }
+            }
+
+            // close reader
+            reader.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
 }
